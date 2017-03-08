279286d98954a3e79ed51db182c64be1078a07f0
Erik Olsson <eriol726@student.liu.se>
2017-03-08 09:52:33 +0100

    Neighbor Faces are selected

--- polySelector.py
+++ polySelector.py
@@ -55,10 +55,6 @@ while not meshPolyIt.isDone():
 def getUpFace(faceIndex):
     poly_ids = []
     poly_ids = ui_setTargetGeometry()
-    poly_ids[0] = poly_ids[0]+1 
-    poly_ids[1] = poly_ids[1]+1 
-    poly_ids[2] = poly_ids[2]+1 
-    poly_ids[3] = poly_ids[3]+1 
 
     sel = om.MSelectionList()
     om.MGlobal.getActiveSelectionList(sel)
@@ -66,7 +62,7 @@ def getUpFace(faceIndex):
     mdag = om.MDagPath()
     sel.getDagPath(0, mdag)
 
-    print  poly_ids[0]
+    print "poly_ids: ",  poly_ids[0]
 
     # Create an MIntArray and populate it with component ids to add to our component object
     # MIntArray takes an array of ints. That has to be passed using an MScriptUtil pointer
@@ -121,12 +117,16 @@ def ui_setTargetGeometry():
     targetGeom = []
 
     selList = om.MSelectionList()
+    #lägger till markerade faces i listan selList
     om.MGlobal.getActiveSelectionList(selList)
 
     if selList.isEmpty():
         print "no targets"
         return
 
+    
+
+    # itererar igenom listan med markerade faces
     selListIter = om.MItSelectionList(selList)
     while not selListIter.isDone():
 
@@ -141,20 +141,133 @@ def ui_setTargetGeometry():
         compListFn = om.MFnComponent(components)
         compType = compListFn.componentType()
 
+        # kontrollerar om det finns ett polygon i komponenterna
         if compType == om.MFn.kMeshPolygonComponent:
+        	# styckar ut en enstaka komponent från alla markerade komponenter
             compListFn = om.MFnSingleIndexedComponent(components)
             ids = om.MIntArray()
+            # äntligen får vi ut face id:et
             compListFn.getElements(ids)
             selItem = SelectionItem(dagPath, ids)
+            # lägger till den markerade facet/componenten i en lista
             targetGeom.append(selItem)
 
         selListIter.next()
+
     if not targetGeom :
         print "somthing is wrong" 
         return
 
+
+    #for obj in targetGeom :
+    print "obj: " + str(targetGeom[0].polyIds[0])
+
     print "targets: " + str(ids)
-    return ids
+
+    
+
+    connectedFaces = om.MIntArray()
+    faceIndecis = om.MIntArray()
+    polyEdgesIds = om.MIntArray()
+    dummy = om.MScriptUtil()
+    edgeFaces = om.MIntArray()
+    dummyIntPtr = dummy.asIntPtr()
+    polyData = []
+     #MStatus status
+    # objList är target polygon
+    for obj in targetGeom :
+    	edgeIter = om.MItMeshEdge(obj.dagPath)
+    	faceIter = om.MItMeshPolygon(obj.dagPath)
+
+    	# skapar ett nytt klassobjekt
+    	objPolyData = ObjectPolyData()
+    	# lägger till klassobjektet i listan
+    	polyData.append(objPolyData)
+    	# initierar dagPath
+        objPolyData.dagPath = obj.dagPath
+        #initierar faceIter.count() antal toma polygons till objektet
+    	objPolyData.polygons = [PolyData() for _ in xrange(faceIter.count())]
+
+    	objPolyData.polygons[20].selected = True
+    	#print "edgeIter: " + str(edgeIter.index())
+    	# här loopar vi igenom alla edges i meshen
+    	i=0
+        while not faceIter.isDone():
+            #print "index: ", faceIter.index()
+            #cfLength = edgeIter.getConnectedFaces(connectedFaces)
+            polyData = objPolyData.polygons[i]
+
+            # itererar igenom alla edges
+            faceIter.getEdges(polyEdgesIds)
+
+            for edgeId in polyEdgesIds:
+
+                    edgeIter.setIndex(edgeId, dummyIntPtr)
+                    cfLength = edgeIter.getConnectedFaces(edgeFaces)
+
+                    if cfLength == 2 :
+                    	# bestämmer vilket face utav två index som ska tilldelas med 1.0
+                        otherFace = edgeFaces[1] if edgeFaces[0] == i else edgeFaces[0]
+                        polyData.connectedFaces[otherFace] = 1.0
+
+            
+            i += 1
+            faceIter.next()
+        
+
+    print "Poly: ", len(objPolyData.polygons)
+
+    neighborFaces = []
+    for x in range(0, 3):
+	    for key in objPolyData.polygons[ids[x]].connectedFaces:
+	            neighborFaces.append(key)
+
+
+    print "NeighborFaces: " + str(neighborFaces)
+
+    # for index in ids:
+    # 	objPolyData.polygons[ids[0]]
+
+    # neighbors = 
+
+    return neighborFaces
+
+#######################################################################################################################
+
+class PolyData:
+
+#----------------------------------------------------------------------------------------------------------------------
+    def __init__(self):
+        self.selected = False
+        self.connectedFaces = {}
+
+#----------------------------------------------------------------------------------------------------------------------
+    def __str__(self):
+        endStr = '\n'
+        outStr = ''
+        outStr += ', selected = ' + str(self.selected) + endStr
+        for key in self.connectedFaces:
+            outStr += '[' + str(key) + '] = ' + str(self.connectedFaces[key]) + endStr
+        return outStr
+
+#######################################################################################################################
+
+class ObjectPolyData:
+
+#----------------------------------------------------------------------------------------------------------------------
+    def __init__(self):
+        self.dagPath = None
+        self.polygons = []
+
+#----------------------------------------------------------------------------------------------------------------------
+    def __str__(self):
+        endStr = '\n'
+        outStr = ''
+        outStr += 'DagPath = ' + self.dagPath.fullPathName() + endStr
+        for i, poly in enumerate(self.polygons):
+            outStr += 'polyId = ' + str(i) + endStr
+            outStr += str(poly) + endStr
+        return outStr  
 
 class SelectionItem:
 
